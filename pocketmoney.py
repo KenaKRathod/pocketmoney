ee1183a2d2a7ea02ff3a5dedd69fd86c5a078766
Kena K rathod <70839921+KenaKRathod@users.noreply.github.com>
2021-11-18 18:53:16 +0530

    Add files via upload

---
+++ pocketmoney.py
@@ -0,0 +1,10 @@
+pocketmoney = int(input("enter the pocket money u get:"))
+if (pocketmoney>500):
+    print("you are a rich kid")
+elif (pocketmoney>100):
+    print("you have a good life")
+else:
+    print("i know what u feel")
+
+
+
